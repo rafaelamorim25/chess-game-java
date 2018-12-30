4fc547277ec59f60e5e99e366ceb999fbcecced2
Rafael Amorim <rafaelamorimclash@gmail.com>
2018-12-30 00:16:09 -0200

    Classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 	
 	public static void main(String[] args) {
-		System.out.println("Oi");
+		Position p = new Position(4, 2);
+		System.out.println(p);
 	}
 
 }
