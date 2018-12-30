d9a044bf77d313f5dc5d8b46d9afadb0097382b6
Rafael Amorim <rafaelamorimclash@gmail.com>
2018-12-30 19:17:22 -0200

    Printing impossible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,11 +27,17 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		this.validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
-		validateSourcePosition(source);
-		validateTargetPosition(source, target);
+		this.validateSourcePosition(source);
+		this.validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece; 
 	}
